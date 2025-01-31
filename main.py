833f7cadf4bfe9969d93f353d967c11d2a4f0ea7
Rostislaw127 <ronyajordan12@gmail.com>
2025-01-31 17:17:49 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import ( QLabel, QApplication, QWidget, QVBoxLayout, QHBoxLayout, QPushButton)
+from PyQt5.QtGui import QPixmap
+from random import choice
+picture ['kamin.jpg','noj.jpg', 'papir.jpg']
+
+def comp():
+    13.setPixmap (QPixmap(choice(picture)))
+
+def click1():
+    pixmap QPixmap('kamin.jpg')
+    14.setPixmap(pixmap) 
+    comp()
+
+def click2():
+    pixmap - QPixmap('noj.jpg')
+    14.setPixmap(pixmap)
+    comp()
+
+def click3():
+    pixmap - QPixmap('papir.jpg')
+    14.setPixmap(pixmap)
+    comp()
+
+app QApplication([])
